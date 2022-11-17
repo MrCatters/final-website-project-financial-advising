beea8451908e8565b1ea5f59bab72ff737f407d2
MrCatters <73203569+MrCatters@users.noreply.github.com>
2022-11-17 12:43:59 -0500

    Fixed duplicate files in root directory

--- elements.js
+++
@@ -1,7 +0,0 @@
-// https://learn.jquery.com/using-jquery-core/document-ready/
-// Load the Nav bar
-$(document).ready(function () {
-    $(".nav-bar").load("nav.html");
-    $(".footer").load("footer.html");
-});
-
