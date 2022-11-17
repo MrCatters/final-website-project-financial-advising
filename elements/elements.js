21e392e82956ff5befb62d45ae33f58035027b52
MrCatters <73203569+MrCatters@users.noreply.github.com>
2022-11-17 16:25:11 -0500

    Fixed directory traversal when transfered to the server

--- elements/elements.js
+++ elements/elements.js
@@ -1,7 +1,7 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 // Load the Nav bar
 $(document).ready(function () {
-    $(".nav-bar").load("/nav/nav.html");
-    $(".footer").load("/footer/footer.html");
+    $(".nav-bar").load("../nav/nav.html");
+    $(".footer").load("../footer/footer.html");
 });
 
